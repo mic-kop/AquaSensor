624e36ec1f5f9bbf7f108e9a9b1159884e0b5e66
mic-kop <kopczykmk@gmail.com>
2017-06-09 17:28:40 +0200

    .

--- AquaSensor/AquaSensor/AquaSensorWindow.cs
+++ AquaSensor/AquaSensor/AquaSensorWindow.cs
@@ -15,7 +15,7 @@ namespace AquaSensor
     public partial class AquaSensorWindow : Form
     {
         SerialPort sp = new SerialPort();
-        int i = 0;
+        long i = 0;
         string x;
         int wart;
         public AquaSensorWindow()
@@ -24,65 +24,73 @@ namespace AquaSensor
             sp.PortName = "COM3";
             sp.BaudRate = 9600;
             chart1.Series.Add("series1");
-        //chart1.Series["series1"].Color = Color.Red;
-    }
+            chart1.Series["series1"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["series1"].BorderWidth = 1;
+            //chart1.Series["series1"].Color = Color.Red;
+        }
   //  public static SystemSound Beep { get; }
 
     private void timer1_Tick(object sender, EventArgs e)
         {
             i++;
-            try
-            {
-                x = sp.ReadLine();
-                label3.Text = x;
-                wart = int.Parse(x);
-
-               if (1050 - wart > 0)
-               {
-                        chart1.Series["series1"].Points.AddXY(i, 1050 - wart);
-                if (1050 - wart < 100)
-                {
-                    chart1.Series["series1"].Color = Color.Red;
-
-                        //System.Media.SystemSounds.Beep.Play();
-                        //System.Media.SystemSounds.Asterisk.Play();
-                        try
-                        {
-                            SoundPlayer simpleSound = new SoundPlayer(@"‪alert.wav");
-                            simpleSound.Play();
-                        }catch {
-
-                        }
-                    }
-                else
-                {
-                    chart1.Series["series1"].Color = Color.Green;
-                }
-
-
-                if (1050-wart > 300)
-                    label3.ForeColor = System.Drawing.Color.LightGreen;
-                else if (1050 - wart > 200)
-                    label3.ForeColor = System.Drawing.Color.Yellow;
-                else if (1050 - wart > 100)
-                    label3.ForeColor = System.Drawing.Color.IndianRed;
-                else if (1050 - wart > 60)
-                    label3.ForeColor = System.Drawing.Color.Red;
-                else
-                    label3.ForeColor = System.Drawing.Color.DarkRed;
-            }
-            }
-            catch
-            {
-                label3.Text = "NO SIGNAL";
+          
                 try
                 {
-                    sp.Open();
+                    x = sp.ReadLine();
+
+                    wart = int.Parse(x);
+
+                    label3.Text = (1050-wart).ToString();
+
+                    if (1050 - wart > 0)
+                    {
+                        chart1.Series["series1"].Points.AddXY(i / 60, 1050 - wart);
+                        if (1050 - wart < 100)
+                        {
+                            chart1.Series["series1"].Color = Color.Red;
+
+                            //System.Media.SystemSounds.Beep.Play();
+                            //System.Media.SystemSounds.Asterisk.Play();
+                            try
+                            {
+                                SoundPlayer simpleSound = new SoundPlayer(@"‪alert.wav");
+                                simpleSound.Play();
+                            }
+                            catch
+                            {
+
+                            }
+                        }
+                        else
+                        {
+                            chart1.Series["series1"].Color = Color.Green;
+                        }
+
+
+                        if (1050 - wart > 300)
+                            label3.ForeColor = System.Drawing.Color.LightGreen;
+                        else if (1050 - wart > 200)
+                            label3.ForeColor = System.Drawing.Color.Yellow;
+                        else if (1050 - wart > 100)
+                            label3.ForeColor = System.Drawing.Color.IndianRed;
+                        else if (1050 - wart > 60)
+                            label3.ForeColor = System.Drawing.Color.Red;
+                        else
+                            label3.ForeColor = System.Drawing.Color.DarkRed;
+                    }
                 }
                 catch
                 {
+                    label3.Text = "NO SIGNAL";
+                    try
+                    {
+                        sp.Open();
+                    }
+                    catch
+                    {
 
-                }
+                    }
+                
             }
         }
 
